6e81ef966be29021ad09c907dd0583bc28fe39be
Simon Sekavčnik <simon.sekavcnik@tum.de>
2024-02-05 10:45:33 +0100

    update

--- emacs/init.el
+++ emacs/init.el
@@ -8,10 +8,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
- '(org-agenda-files '("~/.config/emacs/config.org")))
+ '(org-agenda-files
+   '("~/.roam/20230914123915-inbox.org" "/home/simon/.config/emacs/config.org"))
+ '(package-selected-packages '(hydra)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-drill-cloze-face ((t (:foreground "red" :weight bold))))
+ '(org-drill-done-cloze-face ((t (:foreground "green" :weight bold)))))
